b81eeee05e16d7e3dc8c499239a76702b46c940a
b3hzad shabani <b3hzadsh@gmail.com>
2020-08-28 21:43:58 +0430

    remove  staged animation

--- lib/screens/choosing_cat_screen.dart
+++ lib/screens/choosing_cat_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:show_up_animation/show_up_animation.dart';
 import 'package:flutter_app/widgets/cat_widget.dart';
 import 'package:nice_button/nice_button.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+// import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 // import 'package:shared_preferences/shared_preferences.dart';
 
 /* void setPerfs() async {
@@ -24,94 +25,94 @@ class _ChoosingCatScreenState extends State<ChoosingCatScreen> {
         centerTitle: false,
       ),
       body: Container(
+        color: Colors.white12,
         // alignment: Alignment.topCenter,
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(18.0),
-            child: AnimationLimiter(
-              child: Column(
-                children: AnimationConfiguration.toStaggeredList(
-                  duration: const Duration(milliseconds: 775),
+            // child: AnimationLimiter(
+            child: Column(
+              /* children: AnimationConfiguration.toStaggeredList(
+                  duration: const Duration(milliseconds: 275),
                   childAnimationBuilder: (widget) => SlideAnimation(
                     horizontalOffset: 70.0,
                     child: FadeInAnimation(
                       child: widget,
                     ),
-                  ),
+                  ), */
+              children: [
+                Text(
+                  "موضوعات مورد علاقه خود را انتخاب و دکمه ثبت را فشار دهید",
+                  textAlign: TextAlign.center,
+                ),
+                Wrap(
+                  // crossAxisAlignment: WrapCrossAlignment.center,
+                  spacing: 10,
+                  runSpacing: 10,
                   children: [
-                    Text(
-                      "موضوعات مورد علاقه خود را انتخاب و دکمه ثبت را فشار دهید",
-                      textAlign: TextAlign.center,
+                    CatCard(
+                      cardColor: Colors.white,
+                      title: "ورزشی",
+                      imageUrl: "assets/images/ball.png",
+                      // sport is 0
                     ),
-                    Wrap(
-                      // crossAxisAlignment: WrapCrossAlignment.center,
-                      spacing: 10,
-                      runSpacing: 10,
-                      children: [
-                        CatCard(
-                          cardColor: Colors.white,
-                          title: "ورزشی",
-                          imageUrl: "assets/images/ball.png",
-                          // sport is 0
-                        ),
-                        CatCard(
-                          cardColor: Colors.white,
-                          title: "تکنولوژی",
-                          imageUrl: "assets/images/IT.jpg",
-                          // it is 1
-                        ),
-                        CatCard(
-                          cardColor: Colors.white,
-                          title: "دانلود",
-                          imageUrl: "assets/images/dl.png",
-                          // dl is 2
-                        ),
-                        CatCard(
-                          cardColor: Colors.white,
-                          title: "بازی",
-                          imageUrl: "assets/images/game.png",
-                          // game is 3
-                        ),
-                        CatCard(
-                          cardColor: Colors.white,
-                          title: "فیلم و سریال",
-                          imageUrl: "assets/images/movie.png",
-                          // movie is 4
-                        ),
-                        CatCard(
-                          cardColor: Colors.white,
-                          title: "اقتصاد",
-                          imageUrl: "assets/images/economy.png",
-                          // movie is 4
-                        ),
-                        // add economy politic
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Container(
-                            /* height: MediaQuery.of(context).size.width / 3,
+                    CatCard(
+                      cardColor: Colors.white,
+                      title: "تکنولوژی",
+                      imageUrl: "assets/images/IT.jpg",
+                      // it is 1
+                    ),
+                    CatCard(
+                      cardColor: Colors.white,
+                      title: "دانلود",
+                      imageUrl: "assets/images/dl.png",
+                      // dl is 2
+                    ),
+                    CatCard(
+                      cardColor: Colors.white,
+                      title: "بازی",
+                      imageUrl: "assets/images/game.png",
+                      // game is 3
+                    ),
+                    CatCard(
+                      cardColor: Colors.white,
+                      title: "فیلم و سریال",
+                      imageUrl: "assets/images/movie.png",
+                      // movie is 4
+                    ),
+                    CatCard(
+                      cardColor: Colors.white,
+                      title: "اقتصاد",
+                      imageUrl: "assets/images/economy.png",
+                      // movie is 4
+                    ),
+                    // add economy politic
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Container(
+                        /* height: MediaQuery.of(context).size.width / 3,
                           width: MediaQuery.of(context).size.width / 3, */
-                            child: NiceButton(
-                              width: MediaQuery.of(context).size.width / 2,
-                              elevation: 2.0,
-                              radius: 52.0,
-                              text: "ثبت",
-                              gradientColors: [secondColor, firstColor],
-                              background: Colors.blueAccent,
-                              onPressed: () {
-                                print("hello");
-                              },
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+                        child: NiceButton(
+                          width: MediaQuery.of(context).size.width / 2,
+                          elevation: 2.0,
+                          radius: 52.0,
+                          text: "ثبت",
+                          gradientColors: [secondColor, firstColor],
+                          background: Colors.blueAccent,
+                          onPressed: () {
+                            print("hello");
+                          },
+                        ),
+                      ),
+                    )
                   ],
                 ),
-              ),
+              ], /* 
+                ),
+              ), */
             ),
           ),
         ),
-        color: Colors.white12,
       ),
     );
   }
